033396bebc3e977ca9357332413e9217a8674864
HeberFHLemes <hfhlemes@gmail.com>
2025-08-27 18:41:31 -0300

    feat: implementando headers e footers, descricao em produto, melhor estilização.

--- src/main/java/com/grupo6/delivery/controller/CardapioController.java
+++ src/main/java/com/grupo6/delivery/controller/CardapioController.java
@@ -1,15 +1,11 @@
 package com.grupo6.delivery.controller;
 
 import com.grupo6.delivery.service.ProdutoService;
-import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 
 @Controller
--- src/main/java/com/grupo6/delivery/service/ProdutoService.java
+++ src/main/java/com/grupo6/delivery/service/ProdutoService.java
@@ -5,17 +5,19 @@ import com.grupo6.delivery.model.Produto;
 import com.grupo6.delivery.model.TipoIngrediente;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ProdutoService {
 
     public Produto[] mockProdutos(){
         return new Produto[]{
-                new Produto("X-Tudo", 20.99d, "xtudo.jpg", Map.of(
+                new Produto("X-Tudo", "O Hambúrguer mais completo de Caxias!", 20.99d, "xtudo.jpg", Map.of(
                         new Ingrediente("Pão", TipoIngrediente.PAO, "UN"), 1d,
                         new Ingrediente("Carne", TipoIngrediente.CARNE, "g"), 200d,
                         new Ingrediente("Alface", TipoIngrediente.SALADA, "g"), 5d,
@@ -24,12 +26,12 @@ public class ProdutoService {
                         new Ingrediente("Queijo", TipoIngrediente.LATICINIO, "UN"), 2d,
                         new Ingrediente("Cebola", TipoIngrediente.SALADA, "g"), 10d
                 )),
-                new Produto("Batata-Frita", 6.99d, "batata-frita.jpg", Map.of(
+                new Produto("Batata-Frita", "Porção de batata-frita média", 6.99d, "batata-frita.jpg", Map.of(
                         new Ingrediente("Batata-Frita", TipoIngrediente.BATATA, "g"), 42.5d,
                         new Ingrediente("Ketchup", TipoIngrediente.MOLHO, "UN"), 1d,
                         new Ingrediente("Maionese", TipoIngrediente.MOLHO, "UN"), 1d
                 )),
-                new Produto("Pepsi 600ml", 7.99d, "cola.jpg", Map.of(
+                new Produto("Pepsi 600ml", "Refrigerante Pepsi de 600ml", 7.99d, "cola.jpg", Map.of(
                         new Ingrediente("Refrigerante", TipoIngrediente.BEBIDA, "L"), 0.6d
                 ))
         };
